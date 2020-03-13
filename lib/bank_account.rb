ab3be5f2183e0ab3a0e014d09d2761d3faf66886
kaidornel <kai_dornel@hotmail.com>
2020-03-12 21:46:07 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,73 @@
+require 'pry'
+
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+    def initialize (name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(money)
+        @balance += money
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @status == "open" && @balance > 0
+    end
+
+    def close_account
+        @status = "closed"
+    end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class BankAccount
+
+#     attr_reader :name, :bank_account, :balance, :status
+#     def initialize(name)
+#         @bank_account = bank_account
+#         @name = name
+#         @balance = 1000
+#         @status = 'open'
+#     end
+
+#     def deposit(money)
+#         @balance += money
+#     end
+
+#     def display_balance
+#         "Your balance is $#{@balance}."
+#     end
+
+#     def valid?
+#         @status
+#         @balance > 0
+#         # binding.pry
+#     end
+# end
